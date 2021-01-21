f22eb73d862d6d991619367b520b753741e0164c
AragonMoreyraSaulRenato <armhahunter21@gmail.com>
2021-01-20 21:54:41 -0600

    add home page temp

--- lib/src/pages/home_temp.dart
+++ lib/src/pages/home_temp.dart
@@ -1,8 +1,46 @@
 import 'package:flutter/material.dart';
 
 class HomePageTemp extends StatelessWidget {
+  final opciones = ['Un', 'Dos', 'Tres', 'Cuatro'];
+
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text('Hola mundo'));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Componentes ListView'),
+      ),
+      body: ListView(
+        //children: _crearItems(),
+        children: _crearItemShort(), //
+      ),
+    );
+  }
+
+  List<Widget> _crearItems() {
+    List<Widget> list = new List<Widget>();
+    for (String item in opciones) {
+      final tempWidget = ListTile(
+        title: Text(item),
+      );
+      list..add(tempWidget)..add(Divider());
+    }
+    return list;
+  }
+
+  List<Widget> _crearItemShort() {
+    return opciones.map((item) {
+      return Column(
+        children: [
+          ListTile(
+            title: Text(item),
+            subtitle: Text('Holaaaaaaaa'),
+            leading: Icon(Icons.accessibility_new),
+            trailing: Icon(Icons.arrow_right),
+            onTap: () {},
+          ),
+          Divider()
+        ],
+      );
+    }).toList();
   }
 }
