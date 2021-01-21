a1d0c59ed3ec09ad59354c781277def579b55c30
AragonMoreyraSaulRenato <armhahunter21@gmail.com>
2021-01-20 21:34:13 -0600

    inicialize app

---
+++ android/app/src/main/kotlin/com/example/use_many_widgets_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.use_many_widgets_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:use_many_widgets_flutter/src/pages/home_temp.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Componentes App'),
+          ),
+          body: HomePageTemp()),
+    );
+  }
+}
