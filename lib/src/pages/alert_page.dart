6acaf1d8578be789e9d118f9f842d523ee21c08c
AragonMoreyraSaulRenato <armhahunter21@gmail.com>
2021-01-26 15:36:30 -0700

    agrego alert

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -7,11 +7,54 @@ class AlertPage extends StatelessWidget {
         appBar: AppBar(
           title: Text('Alert Page'),
         ),
+        body: Center(
+          child: RaisedButton(
+            child: Text('Mostrar Alerta'),
+            color: Colors.blue,
+            textColor: Colors.white,
+            shape: StadiumBorder(),
+            onPressed: () => _mostrarAlerta(context),
+          ),
+        ),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.arrow_back),
           onPressed: () {
             Navigator.pop(context);
           },
         ));
+  }
+
+  void _mostrarAlerta(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          title: Text('Titulo'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text('Este es el contenido de las caja de alerta'),
+              FlutterLogo(size: 100.0)
+            ],
+          ),
+          actions: <Widget>[
+            FlatButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text('Cancelar'),
+            ),
+            FlatButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Ok'),
+            ),
+          ],
+        );
+      },
+    );
   }
 }
